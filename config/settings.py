7dc224863f4b15562d1f05f2948218af122e86f6
YL <syl000126@naver.com>
2021-10-31 20:45:25 +0900

    edit2

--- config/settings.py
+++ config/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR, 'templates'], # Django 3.x
+        'DIRS': [BASE_DIR / 'templates'], # Django 3.x
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
